4650f51f53d8db4e3770f42518dbdd0061ba1f50
Your Name <you@example.com>
2025-09-02 17:48:30 +0300

    created a new file

---
+++ ap-capstone/app/Models/Participant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Participant extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'full_name',
+        'email',
+        'affiliation',
+        'specialization',
+        'cross_skill_trained',
+        'institution'
+    ];
+    protected $casts = ['cross_skill_trained' => 'boolean'];
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class, 'project_participant')
+            ->withPivot(['role_on_project', 'skill_role'])
+            ->withTimestamps();
+    }
+}
