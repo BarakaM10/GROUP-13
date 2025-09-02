a1c20460cd8e239b876d6ddb6e6c0d89c9fbbd71
jordanmwesigwa <jordanssemuyimba@gmail.com>
2025-09-02 21:53:05 +0300

    register projects resource routes

--- ap-capstone/routes/web.php
+++ ap-capstone/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
+
+Route::resource('projects', ProjectController::class);
