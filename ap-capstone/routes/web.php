59b0e21b0b325c695cc9b3763c9e39a0346760e7
JashobeamKayiwa <jashobeammagezi@gmail.com>
2025-09-03 00:37:29 +0300

    Update web routes

--- ap-capstone/routes/web.php
+++ ap-capstone/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FacilityController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
