ecb60a6368e7a8a67815dacfc61f4ba0dd68d743
JashobeamKayiwa <jashobeammagezi@gmail.com>
2025-09-03 00:06:29 +0300

    Add HomeController, home view, and layout templates

--- ap-capstone/routes/web.php
+++ ap-capstone/routes/web.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FacilityController;
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::resource('facilities', FacilityController::class);
